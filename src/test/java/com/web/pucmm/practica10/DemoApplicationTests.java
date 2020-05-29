d1d708eef4270bdc79329fccb4168225b278d347
lyriad <manueleduardo0320@gmail.com>
2020-05-29 10:03:22 -0400

    init

---
+++ src/test/java/com/web/pucmm/practica10/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.pucmm.practica10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
